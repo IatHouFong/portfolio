e793ab5b69be3758344d8bcbef42640abdd687e0
Keven Fong <69735343+kevinih11@users.noreply.github.com>
2020-12-17 22:58:20 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,151 @@
+$(window).on("load", function(){
+
+	$(".loader .inner").fadeOut(500, function(){
+		$(".loader").fadeOut(750);
+	});	
+
+	$(".items").isotope({
+		filter: '*',
+		animationOption: {
+			duration: 1350,
+			easing: 'linear',
+			queue: false
+		}
+	});
+})
+
+$(document).ready(function() {
+
+	//slides same as <div id="slides">
+	//function from github
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 3000,
+		pagination: false
+	});
+
+	//function from github
+	var typed = new Typed(".typed", {
+		strings: [ "Mechanical Engineer.", "Robotics.", "Semiconductor.", "Web Developer.", "Musician."],
+		typeSpeed: 90,
+		loop: true,
+		startDelay: 950,
+		showCursor: false
+	});
+
+	//function from github
+	$('.owl-carousel').owlCarousel({
+	    loop: true,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        autoplayHoverPause: false,
+        autoplaySpeed: 1400,
+	    items: 8,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+		}
+
+
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function(){
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			})
+
+			countUpFinished = true;
+		}
+
+
+	});
+
+	$("[data-fancybox]").fancybox();
+
+
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOption: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+
+	});
+
+
+	$("#navigation li a").click(function(e){
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50}, "slow");
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation(){
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else{
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
+
+});
+
+
+	
